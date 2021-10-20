f01639aaa41c7b0294e0e3bff953a90aa99a675b
Lola Rufino <lola.ruf.ar@gmail.com>
2021-10-20 11:00:37 +0200

    Create Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="main-wrapper">
+    <h1 class="title">Quiz Game</h1>
+    <p class="text">
+      Here you have a quiz game to practice some coding interview questions.
+      Good luck! 🍀
+    </p>
+    <img
+      src="https://i.ibb.co/f9zG1Jm/animation-640-kuxzpiiebig.gif"
+      alt="Person thinking gif"
+    />
+    <router-link to="/questions"
+      ><button class="button">Let's play!</button></router-link
+    >
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import { defineComponent } from "vue";
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  name: "Home",
 });
 </script>
+
+<style lang="scss">
+@import "../styles/variables.scss";
+.main-wrapper {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  .title {
+    font-family: $titlefont;
+    font-size: 48px;
+    color: $maincolor;
+    text-shadow: 3px 3px white, 5px 5px $maincolor;
+  }
+  .text {
+    font-family: $bodyfont;
+    font-size: 16px;
+    color: darkgray;
+  }
+  .button {
+    padding: 10px;
+    font-size: 24px;
+    font-family: $titlefont;
+    background-color: $maincolor;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: all 300ms ease-in-out;
+    &:hover {
+      transform: translate(-4px, -4px);
+      box-shadow: 2px 2px white, 4px 4px $maincolor;
+    }
+  }
+}
+</style>

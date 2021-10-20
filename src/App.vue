285cf38e5c8f1285584cfe5a40cae1707046e8e2
Lola Rufino <lola.ruf.ar@gmail.com>
2021-10-20 13:24:58 +0200

    Create Header component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <router-view />
+  <div class="wrapper">
+    <Header />
+    <router-view />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Header from "../src/components/Header.vue";
 
 export default defineComponent({
   name: "App",
+  components: {
+    Header,
+  },
 });
 </script>
 
@@ -15,5 +22,13 @@ export default defineComponent({
 * {
   margin: 0;
   padding: 0;
+}
+.wrapper {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>

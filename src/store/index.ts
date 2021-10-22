04340edff14b42560f4cd278d0b3b1928ebf1ad8
Lola Rufino <lola.ruf.ar@gmail.com>
2021-10-22 17:31:27 +0200

    Create action and mutation for questions array

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,22 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
+    questions: [
+    ]
   },
   mutations: {
+    getQuestions(state, payload){
+      const questionsArray = payload.map((question: any) => { return {question: question.question, answers: {...question.incorrect_answers, correct: question.correct_answer}, correctAnswer: "correct"}});
+      state.questions = questionsArray;
+    }
   },
   actions: {
+async fetchQuestionsFromApi ({commit}) {
+  const {data}:any = await axios.get('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple&encode=url3986')
+  commit('getQuestions', data.results)
+}
   },
   modules: {
   },
